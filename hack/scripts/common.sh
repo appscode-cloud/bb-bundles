aa08ad0377ff9e95b4e697eab5d387c9680807b3
Tamal Saha <tamal@appscode.com>
2020-06-25 10:40:07 -0700

    Update chart config
    Signed-off-by: Tamal Saha <tamal@appscode.com>

--- hack/scripts/common.sh
+++ hack/scripts/common.sh
@@ -14,11 +14,11 @@
 # See the License for the specific language governing permissions and
 # limitations under the License.
 
-GCP_PROJECT=appscode-domains
-BUCKET=bytebuilders-bundles
+GCP_PROJECT=
+BUCKET=ci-charts
 
-REPO_DOMAIN=bundles.byte.builders
-REPO_DIR=stable
+REPO_DOMAIN=ci-charts.storage.googleapis.com
+REPO_DIR=bundles
 REPO_URL=https://${REPO_DOMAIN}/${REPO_DIR}/
 
 # ref: https://gist.github.com/joshisa/297b0bc1ec0dcdda0d1625029711fa24
